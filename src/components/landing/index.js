35dc3d674693af3551fd700115cbf1c96361da2d
Zachary <kimurayu106@outlook.com>
2024-06-18 19:05:27 +0300

    style: add the rooms bar

--- src/components/landing/index.js
+++ src/components/landing/index.js
@@ -9,16 +9,8 @@ import Slide3 from "../../assets/3.jpg";
 import PatterImg from "../../assets/hotel.jpg"
 import GuestRoom from "../../assets/guest.jpg";
 import IntroImg from "../../assets/checkin.png";
-import Member1 from "../../assets/member1.png";
-import Member2 from "../../assets/member2.png";
-import Member3 from "../../assets/member3.png";
+import Rooms from "../../constracts/Rooms";
 import './Landing.scss';
-
-const Members = [
-    { name: "Ingrid Naiman", src: Member1, content: "I have developed a complete line of herbal products and essential oils to recover from mold exposure." },
-    { name: "Hiroki Moto", src: Member2, content: "I am assuring the availability of digital materials needed to recover." },
-    { name: "Balaji Sampath", src: Member3, content: "I am administering the Locals.com community." }
-];
 
 const Landing = () => {
     return (
@@ -63,19 +55,11 @@ const Landing = () => {
                 </div>
             </div>
 
-            <div className="members">
-                <h1>Researchers</h1>
-                <div className="member-card">
-                    {Members.map((item) =>
-                        <Card style={{ width: '18rem' }}>
+            <div className="rooms">
+                <div className="room-card">
+                    {Rooms.map((item) =>
+                        <Card style={{ width: '25rem' }}>
                             <Card.Img variant="top" src={item.src} />
-                            <Card.Body>
-                                <Card.Title>{item.name}</Card.Title>
-                                <Card.Text>
-                                    {item.content}
-                                </Card.Text>
-                                <Button variant="primary">View Details</Button>
-                            </Card.Body>
                         </Card>
                     )}
                 </div>
---
+++ src/constracts/Rooms.js
@@ -0,0 +1,17 @@
+import Room1 from "../assets/member1.png";
+import Room1 from "../assets/member2.png";
+import Room1 from "../assets/member3.png";
+import Room1 from "../assets/member3.png";
+import Room1 from "../assets/member3.png";
+import Room1 from "../assets/member3.png";
+
+const Rooms = [
+    { name: "Ingrid Naiman", src: Room1, content: "I have developed a complete line of herbal products and essential oils to recover from mold exposure." },
+    { name: "Hiroki Moto", src: Room1, content: "I am assuring the availability of digital materials needed to recover." },
+    { name: "Balaji Sampath", src: Room1, content: "I am administering the Locals.com community." },
+    { name: "Balaji Sampath", src: Room1, content: "I am administering the Locals.com community." },
+    { name: "Balaji Sampath", src: Room1, content: "I am administering the Locals.com community." },
+    { name: "Balaji Sampath", src: Room1, content: "I am administering the Locals.com community." }
+];
+
+export default Rooms;
