3c300a4d77653081c712ff245dabb4b2ec447a0c
Zachary <kimurayu106@outlook.com>
2024-06-18 19:07:21 +0300

    style: changed the rooms contracts

--- src/constracts/Rooms.js
+++ src/constracts/Rooms.js
@@ -1,17 +1,17 @@
-import Room1 from "../assets/member1.png";
-import Room1 from "../assets/member2.png";
-import Room1 from "../assets/member3.png";
-import Room1 from "../assets/member3.png";
-import Room1 from "../assets/member3.png";
-import Room1 from "../assets/member3.png";
+import Room1 from "../assets/room1.jpg";
+import Room2 from "../assets/room2.jpg";
+import Room3 from "../assets/room3.jpg";
+import Room4 from "../assets/room4.jpg";
+import Room5 from "../assets/room5.jpg";
+import Room6 from "../assets/room6.jpg";
 
 const Rooms = [
     { name: "Ingrid Naiman", src: Room1, content: "I have developed a complete line of herbal products and essential oils to recover from mold exposure." },
-    { name: "Hiroki Moto", src: Room1, content: "I am assuring the availability of digital materials needed to recover." },
-    { name: "Balaji Sampath", src: Room1, content: "I am administering the Locals.com community." },
-    { name: "Balaji Sampath", src: Room1, content: "I am administering the Locals.com community." },
-    { name: "Balaji Sampath", src: Room1, content: "I am administering the Locals.com community." },
-    { name: "Balaji Sampath", src: Room1, content: "I am administering the Locals.com community." }
+    { name: "Hiroki Moto", src: Room2, content: "I am assuring the availability of digital materials needed to recover." },
+    { name: "Balaji Sampath", src: Room3, content: "I am administering the Locals.com community." },
+    { name: "Balaji Sampath", src: Room4, content: "I am administering the Locals.com community." },
+    { name: "Balaji Sampath", src: Room5, content: "I am administering the Locals.com community." },
+    { name: "Balaji Sampath", src: Room6, content: "I am administering the Locals.com community." }
 ];
 
 export default Rooms;
