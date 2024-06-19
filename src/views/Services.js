2193bd8568cfb86082515b32369a7858536b2c3e
Zachary <kimurayu106@outlook.com>
2024-06-19 18:44:46 +0300

    style: changed the design services

--- src/views/Services.js
+++ src/views/Services.js
@@ -11,16 +11,25 @@ const Services = () => {
     
     return (
         <div className="management">
-            <Carouselfade slide1={Slide1} slide2={Slide2} slide3={Slide3} />
             <div className="goulty">
-                <Image className="image-left" src={PatterImg} rounded/>
+                <Carouselfade slide1={Slide1} slide2={Slide2} slide3={Slide3} />
                 <div className="p-4">
-                    <h1 className="text-yellow">THE GOULTY</h1>
-                    <p className="snow">The Goulty offers traditional and locally inspired cuisine, delicious desserts 
-                        and a selection of fine wines to compliment your meal. Offering panoramic sea views, 
-                        we invite you to have a light snack or a delicious three course meal.
+                    <h1 className="text-yellow">Breakfast</h1>
+                    <p className="snow">A specialty cafe in the heart of Łódź, where you can concentrate, work, 
+                        and eat a nutritious and tasty breakfast or lunch during the day, and enjoy a natural wine from the 
+                        Natural Rascal selection in the evening.
                     </p>
                 </div>
+            </div>
+            <div className="goulty">
+                <div className="p-50">
+                    <h1 className="text-yellow">MISKA GASTRO BOWLS</h1>
+                    <p className="snow">Asian flavours inspired by the culinary art of Thailand are the 
+                        specialty at Miska. A perfect place for lunch or an evening out and open 7 days a week. 
+                        Stop by and explore our menu.
+                    </p>
+                </div>
+                <Image className="image-left" src={PatterImg} />
             </div>
         </div>
     )
