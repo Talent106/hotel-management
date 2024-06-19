c23c969bbc586bdc181b16d7aa1c8a2474b27335
Zachary <kimurayu106@outlook.com>
2024-06-19 21:55:56 +0300

    feature: add the url search hooks useLocation & received the data

--- src/views/RoomSingle.js
+++ src/views/RoomSingle.js
@@ -1,17 +1,43 @@
-import React, { useEffect } from "react";
-import queryFormat from "querystring";
+import React, { useEffect, useState } from "react";
+import { useLocation } from 'react-router-dom';
 import _ from "lodash";
 import Room from "../components/Room";
+import AllRooms from "../constracts/AllRooms";
 
-const RoomSingle = (props) => {
+const RoomSingle = () => {
+    const params = useLocation().search;
+    const [name, setName] = useState(null);
+    const [img, setImg] = useState(null);
+    const [max, setMax] = useState(null);
+    const [bed, setBed] = useState(null);
+    const [price, setPrice] = useState(null);
+    const [reivews, setReviews] = useState(null);
+    const [content, setContent] = useState(null);
 
     useEffect(() => {
-        const params = queryFormat.parse(props.location.search);
-        console("wwwwwww", params);
+        const query = new URLSearchParams(params);
+        if (query.get('ID')) {
+            let room = _.find(AllRooms, function(o) { return o.id == query.get('ID'); });
+            setName(room.name);
+            setImg(room.src);
+            setMax(room.max);
+            setBed(room.bed);
+            setPrice(room.price);
+            setReviews(room.reviews);
+            setContent(room.content);
+        }
     }, []);
 
     return (
-        <Room />
+        <Room
+            name={name}
+            img={img}
+            max={max}
+            bed={bed}
+            price={price}
+            reivews={reivews}
+            content={content}
+        />
     )
 }
 
