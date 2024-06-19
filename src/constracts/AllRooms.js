e04ac8f7a255a722e96fc41542679ef6b269d57f
Zachary <kimurayu106@outlook.com>
2024-06-19 22:20:43 +0300

    style: add the all rooms images and data

--- src/constracts/AllRooms.js
+++ src/constracts/AllRooms.js
@@ -7,6 +7,31 @@ import Room6 from "../assets/rooms/all/6-1.jpg";
 import Room7 from "../assets/rooms/all/7-1.jpg";
 import Room8 from "../assets/rooms/all/8-1.jpg";
 import Room9 from "../assets/rooms/all/9-1.jpg";
+import Room1_2 from "../assets/rooms/all/1-2.jpg";
+import Room1_3 from "../assets/rooms/all/1-3.jpg";
+import Room1_4 from "../assets/rooms/all/1-4.jpg";
+import Room1_5 from "../assets/rooms/all/1-5.jpg";
+import Room2_2 from "../assets/rooms/all/2-2.jpg";
+import Room2_3 from "../assets/rooms/all/2-3.jpg";
+import Room3_2 from "../assets/rooms/all/3-2.jpg";
+import Room3_3 from "../assets/rooms/all/3-3.jpg";
+import Room4_2 from "../assets/rooms/all/4-2.jpg";
+import Room4_3 from "../assets/rooms/all/4-3.jpg";
+import Room4_4 from "../assets/rooms/all/4-4.jpg";
+import Room5_2 from "../assets/rooms/all/5-2.jpg";
+import Room5_3 from "../assets/rooms/all/5-3.jpg";
+import Room5_4 from "../assets/rooms/all/5-4.jpg";
+import Room6_2 from "../assets/rooms/all/6-2.jpg";
+import Room6_3 from "../assets/rooms/all/6-3.jpg";
+import Room6_4 from "../assets/rooms/all/6-4.jpg";
+import Room7_2 from "../assets/rooms/all/7-2.jpg";
+import Room7_3 from "../assets/rooms/all/7-3.jpg";
+import Room7_4 from "../assets/rooms/all/7-4.jpg";
+import Room8_2 from "../assets/rooms/all/8-2.jpg";
+import Room8_3 from "../assets/rooms/all/8-3.jpg";
+import Room9_2 from "../assets/rooms/all/9-2.jpg";
+import Room9_3 from "../assets/rooms/all/9-3.jpg";
+import Room9_4 from "../assets/rooms/all/9-4.jpg";
 
 const AllRooms = [
     {
@@ -17,7 +42,8 @@ const AllRooms = [
         max: 2,
         bed: "King size bed(s) x1",
         reviews: 5,
-        price: 330
+        price: 330,
+        data: {Room1_2, Room1_3, Room1_4, Room1_5}
     },
     {
         id: 2,
@@ -27,6 +53,7 @@ const AllRooms = [
         bed: "King size bed(s) x1",
         price: 400,
         reviews: 4,
+        data: {Room2_2, Room2_3},
         content: "Upgraded bedroom with sea views and balcony, flat-screen TV, complimentary high speed WiFi, Nespresso coffee machine, robes and slippers. Sleeps up to 2 adults."
     },
     {
@@ -37,6 +64,7 @@ const AllRooms = [
         bed: "Double bed(s) x1",
         price: 300,
         reviews: 3,
+        data: {Room3_2, Room3_3},
         content: "Accessible rooms feature wheelchair turning space, alarm cords, wheel-in shower, bathroom grab rails, accessible bed, wardrobe, mirror and writing facilities. Audible and visual or vibrating fire alarms on request or installed."
     },
     {
@@ -47,6 +75,7 @@ const AllRooms = [
         bed: "Double bed(s) x1",
         price: 350,
         reviews: 5,
+        data: {Room4_2, Room4_3, Room4_4},
         content: "Upgraded bedroom with sea view, flat-screen TV, complimentary high speed WiFi, Nespresso coffee machine, robe and slippers. Sleeps 2 adults."
     },
     {
@@ -57,6 +86,7 @@ const AllRooms = [
         bed: "King size bed(s) x2",
         price: 330,
         reviews: 5,
+        data: {Room5_2, Room5_3, Room5_4},
         content: "Upgraded bedroom with sea view, flat-screen TV, complimentary high speed WiFi, Nespresso coffee machine, robe and slippers. Sleeps 2 adults."
     },
     {
@@ -67,6 +97,7 @@ const AllRooms = [
         bed: "King size bed(s) x2",
         price: 330,
         reviews: 4,
+        data: {Room6_2, Room6_3, Room6_4},
         content: "Accessible rooms feature wheelchair turning space, alarm cords, wheel-in shower, bathroom grab rails, accessible bed, wardrobe, mirror and writing facilities. Audible and visual or vibrating fire alarms on request or installed."
     },
     {
@@ -77,6 +108,7 @@ const AllRooms = [
         bed: "King size bed(s) x1",
         price: 320,
         reviews: 3,
+        data: {Room7_2, Room7_3, Room7_4},
         content: "Accessible rooms feature wheelchair turning space, alarm cords, wheel-in shower, bathroom grab rails, accessible bed, wardrobe, mirror and writing facilities. Audible and visual or vibrating fire alarms on request or installed."
     },
     {
@@ -87,6 +119,7 @@ const AllRooms = [
         bed: "King size bed(s) x1",
         price: 380,
         reviews: 2,
+        data: {Room8_2, Room8_3},
         content: "Upgraded bedroom with sea view, flat-screen TV, complimentary high speed WiFi, Nespresso coffee machine, robe and slippers. Sleeps 2 adults."
     },
     {
@@ -97,6 +130,7 @@ const AllRooms = [
         bed: "King size bed(s) x1",
         price: 310,
         reviews: 5,
+        data: {Room9_2, Room9_3, Room9_4},
         content: "Upgraded bedroom with sea view, flat-screen TV, complimentary high speed WiFi, Nespresso coffee machine, robe and slippers. Sleeps 2 adults."
     },
 ];
