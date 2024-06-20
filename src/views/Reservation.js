c51d27ff45a10c2077bc9efc94a113fc206ca1bb
Zachary <kimurayu106@outlook.com>
2024-06-20 12:27:27 +0300

    fix: view Reservation page import use hooks

--- src/views/Reservation.js
+++ src/views/Reservation.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useLocation } from 'react-router-dom';
 import _ from "lodash";
 import Reservate from "../components/Reservate";
