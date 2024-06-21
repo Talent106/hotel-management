2e04babf42e2edea7c40985513fcb8557c553b61
Zachary <kimurayu106@outlook.com>
2024-06-21 22:01:19 +0300

    style: rooms page redesign

--- src/views/Services.js
+++ src/views/Services.js
@@ -6,23 +6,19 @@ import Slide1 from "../assets/services/2.jpg";
 import Slide2 from "../assets/services/3.jpg";
 import Slide3 from "../assets/services/4.jpg";
 import Img5 from "../assets/services/6.jpg";
-import Slide4 from "../assets/4.jpg";
-import Slide5 from "../assets/5.jpg";
-import Slide6 from "../assets/6.jpg";
 import './Services.scss';
 
 const Services = () => {
     
     return (
         <div className="management">
-            <Carouselfade slide1={Slide4} slide2={Slide5} slide3={Slide6}/>
             <div className="goulty">
                 <Image className="image-left" src={PatterImg} />
                 <div className="p-4">
-                    <h1 className="text-yellow">Breakfast</h1>
-                    <p className="snow">A specialty cafe in the heart of Łódź, where you can concentrate, work, 
-                        and eat a nutritious and tasty breakfast or lunch during the day, and enjoy a natural wine from the 
-                        Natural Rascal selection in the evening.
+                    <h1 className="text-yellow">Area</h1>
+                    <p className="snow">At the base of the Kotelnica Białczańska ski lifts, 
+                        this upscale chalet-style hotel in the Tatra Mountains is also a 5-minute 
+                        walk from the Terma Bania water park complex.
                     </p>
                 </div>
             </div>
@@ -31,9 +27,9 @@ const Services = () => {
             <div className="goulty">
                 <div className="p-50 border-right-snow">
                     <h1 className="text-yellow">MISKA GASTRO BOWLS</h1>
-                    <p className="snow">Asian flavours inspired by the culinary art of Thailand are the 
-                        specialty at Miska. A perfect place for lunch or an evening out and open 7 days a week. 
-                        Stop by and explore our menu.
+                    <p className="snow">The right Waiter/Waitress uplifts the dining experience for customers. 
+                        We are looking for someone who will have the patience, personality and perseverance 
+                        to thrive in this role.
                     </p>
                 </div>
                 <Carouselfade slide1={Slide1} slide2={Slide2} slide3={Slide3} />
@@ -41,10 +37,13 @@ const Services = () => {
             <div className="goulty border-top-snow">
                 <Image className="image-left" src={Img5} />
                 <div className="p-50">
-                    <h1 className="text-yellow">SPORTS</h1>
-                    <p className="snow">Asian flavours inspired by the culinary art of Thailand are the 
-                        specialty at Miska. A perfect place for lunch or an evening out and open 7 days a week. 
-                        Stop by and explore our menu.
+                    <h1 className="text-yellow">HONEY MOON</h1>
+                    <p className="snow">The service and hospitality industries have a lot to gain 
+                        from adopting automation for communicating with customers or guests. 
+                        There is an expectation for any quality establishment to have timely and 
+                        clear messaging about availability, bookings, payments and more. It alleviates 
+                        pressures on staff while ensuring patrons get the information they need in a manner 
+                        that suggests a well-run and efficient establishment.
                     </p>
                 </div>
             </div>
